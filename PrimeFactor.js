09c9e234560f4b3a5e1cc99fcd2f55247d61733a
Your Name <you@example.com>
2021-10-01 15:01:21 +0530

    Added : array and map practice problem

--- PrimeFactor.js
+++ PrimeFactor.js
@@ -1,12 +1,22 @@
-let number = 8; 
-let array = new Array();
-for (let i = 2; i*i < number; i++) {
-  while (number % i == 0) {
-    array.push(i);
-    number = number / i;
+var val = 77;
+let checkPrime;
+var primeFactors = new Array();
+for (i = 2; i < val/2; i++) {
+  if (val % i == 0) {
+
+    //check if divisor is prime
+    for (var j = 2; j <= i / 2; j++) {
+      if (i % j == 0) {
+        checkPrime = false;
+      } else {
+        isPrime = true;
+      }
+    }
+    //if divisor is prime
+    if (checkPrime == true) {
+      primeFactors.push(i);
+      val = val/i
+    }
   }
 }
-if (number > 2) {
-  array.push(number);
-}
-console.log(array);
+console.log(primeFactors);
